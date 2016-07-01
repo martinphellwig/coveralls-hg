54f8a2aa92a59a8423885d3de7e98d13304315f6
Martin P. Hellwig <martin.hellwig@gmail.com>
2016-07-01 12:46:08 +0100

    Also print out the coverage data

--- coveralls_hg/coveralls_codeship.py
+++ coveralls_hg/coveralls_codeship.py
@@ -2,11 +2,10 @@
 Use the codeship environmental variables to fill in the right values to upload
 to coveralls.
 """
-
-import os
-from coveralls_hg.api import API
 from pprint import pprint
 from copy import deepcopy
+import os
+from coveralls_hg.api import API
 
 # pylint:disable=dangerous-default-value
 def main(env=os.environ, coverage_file='.coverage'):
@@ -27,8 +26,10 @@ def main(env=os.environ, coverage_file='.coverage'):
     api.set_source_files(coverage_file, strip_path=env['PWD'])
     print('# Uploading to coveralls.io using the following configuration:')
     copied = deepcopy(api.settings['UPLOAD'])
-    copied.pop('source_files')
+    source = copied.pop('source_files')
     pprint(copied)
+    print('# Coverage:')
+    pprint(source)
     api.upload_coverage()
     print('# Upload done.')
 
